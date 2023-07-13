10d5e6787c375944e3fb87216389e43a5b53a56b
bhavik <bhavik@work>
2023-07-12 22:11:46 -0500

    basic list of dynamo tables

--- dojo/aws-sdk-go-sandbox/cmd/main.go
+++ dojo/aws-sdk-go-sandbox/cmd/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+// AWS SDK GO SANDBOX
 func main() {
-	fmt.Println("AWS SDK GO SANDBOX")
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-2"))
+	if err != nil {
+		log.Fatalf("Unable to load SDK config, %v", err)
+	}
+
+	// create dynamoDB client
+	service := dynamodb.NewFromConfig(cfg)
+
+	// Get a list of DynamoDB tables
+	response, err := service.ListTables(context.TODO(), &dynamodb.ListTablesInput{
+		Limit: aws.Int32(5),
+	})
+	if err != nil {
+		log.Fatalf("failed to list tables, %v", err)
+	}
+
+	// List Tables
+	fmt.Println("Tables:")
+	for _, tableName := range response.TableNames {
+		fmt.Println(tableName)
+	}
 }
